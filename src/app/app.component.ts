078d9c69efc346feeb4730f12910c1f0b4859096
DMyslimov <myslimov.danil@wb.ru>
2024-03-26 17:19:54 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  NgxScannerQrcodeComponent,
+  NgxScannerQrcodeModule,
+  ScannerQRCodeConfig,
+  ScannerQRCodeResult
+} from "ngx-scanner-qrcode";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxScannerQrcodeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  data: string = '';
+  @ViewChild('scan') scanComponent!: NgxScannerQrcodeComponent;
   title = 'test-pwa';
+  config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        width: window.innerWidth,
+      },
+    },
+  };
+
+  onScanReady(e: ScannerQRCodeResult[]): void {
+    if (e[0]?.value) {
+      this.data = e[0]?.value;
+      console.log('salam', e[0].value)
+    }
+  }
+
+  ngAfterViewInit() {
+    this.scanComponent.start();
+  }
+
 }
